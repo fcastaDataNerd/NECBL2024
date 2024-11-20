ba73acf8104df3d28997ff6d947d617416dae620
Franco Castagliuolo <168364381+fcastaDataNerd@users.noreply.github.com>
2024-11-19 21:44:31 -0500

    Update fantasy.py

--- fantasy.py
+++ fantasy.py
@@ -3,8 +3,8 @@ import pandas as pd
 import streamlit as st
 pd.set_option('display.max_columns', None)
 # Load data and historical scores
-scores = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\DataMining\\Fantasy.xlsx", sheet_name="ScoringData")
-records = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\DataMining\\Fantasy.xlsx", sheet_name="Records")
-schedule = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\DataMining\\Fantasy.xlsx", sheet_name="Schedule")
-playoffs = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\DataMining\\Fantasy.xlsx", sheet_name="Playoffs")
-print(playoffs)
+scores = pd.read_excel("Fantasy.xlsx", sheet_name="ScoringData")
+records = pd.read_excel(Fantasy.xlsx", sheet_name="Records")
+schedule = pd.read_excel("Fantasy.xlsx", sheet_name="Schedule")
+playoffs = pd.read_excel("Fantast.xlsx", sheet_name="Playoffs")
+st.title("hello")
