4c8d2cb1e1c1573ceab9533800f430d63baf596f
Franco Castagliuolo <168364381+fcastaDataNerd@users.noreply.github.com>
2024-11-20 19:49:37 -0500

    Update playoffs.py

--- playoffs.py
+++ playoffs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 pd.set_option('display.max_columns', None)
 # Load data and historical scores
 scores = pd.read_excel("Fantasy.xlsx", sheet_name="ScoringData")
